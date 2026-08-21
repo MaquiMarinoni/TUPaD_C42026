1c363909912c11ad998fc0ba8a9257b91c9a44a9
MaquiMarinoni <marinonimacarena@gmail.com>
2026-08-20 22:31:44 -0300

    Codigo tp03-un1 finalizado

--- Metodologia-de-sistemas-II/U1/P03/tp3-un1/src/Facturacion.java
+++ Metodologia-de-sistemas-II/U1/P03/tp3-un1/src/Facturacion.java
@@ -2,17 +2,11 @@ import java.util.List;
 
 public class Facturacion {
     public double calcularTotalFacturacion(List<Double> estudios, PerfilPaciente perfil) {
+        // 1. Calculamos el subtotal de los estudios
         double totalFacturado = sumarPrecioEstudios(estudios, perfil);
 
-        if(perfil.tieneObraSocial() == true) {
-            totalFacturado = totalFacturado * 0.9;
-        }
-
-        if(perfil.getCantidadConsultas() > 3) {
-            totalFacturado = totalFacturado - 100;
-        }
-
-        return totalFacturado;
+        // 2. Delegamos la responsabilidad de los descuentos al perfil
+        return perfil.aplicarDescuentosFinales(totalFacturado);
     }
 
     private static double sumarPrecioEstudios(List<Double> estudios, PerfilPaciente perfil) {
--- Metodologia-de-sistemas-II/U1/P03/tp3-un1/src/PerfilPaciente.java
+++ Metodologia-de-sistemas-II/U1/P03/tp3-un1/src/PerfilPaciente.java
@@ -12,4 +12,15 @@ public class PerfilPaciente {
     public boolean aplicaPromocion() { return aplicaPromocion; }
     public boolean tieneObraSocial() { return tieneObraSocial; }
     public int getCantidadConsultas() { return cantidadConsultas; }
+
+    public double aplicarDescuentosFinales(double subtotal) {
+        double total = subtotal;
+        if(this.tieneObraSocial == true) {
+            total = total * 0.9;
+        }
+        if(this.cantidadConsultas > 3) {
+            total = total - 100;
+        }
+        return total;
+    }
 }
